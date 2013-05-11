39dc03dfafd29d70e3e1e8332b6d9cf489705f2f
Jukka Svahn <web@rahinaa.biz>
2013-05-12 02:18:41 +0300

    Bump up the date.

--- rah_backup.php
+++ rah_backup.php
@@ -4,11 +4,10 @@
  * Rah_backup plugin for Textpattern CMS.
  *
  * @author  Jukka Svahn
- * @date    2012-
  * @license GNU GPLv2
  * @link    https://github.com/gocom/rah_backup
  *
- * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
+ * Copyright (C) 2013 Jukka Svahn <http://rahforum.biz>
  * Licensed under GNU General Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
