d347be605943172f04b23f8235e51cc48b89b8e8
Jukka Svahn <web@rahinaa.biz>
2013-05-12 02:18:23 +0300

    Typo.

--- rah_backup.php
+++ rah_backup.php
@@ -9,7 +9,7 @@
  * @link    https://github.com/gocom/rah_backup
  *
  * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
- * Licensed under GNU Genral Public License version 2
+ * Licensed under GNU General Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
