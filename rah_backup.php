5ddfa5623b4bce84d50d765e354f231cf549fe02
Jukka Svahn <web@rahinaa.biz>
2013-05-13 09:43:52 +0300

    Erroneous syntax.

--- rah_backup.php
+++ rah_backup.php
@@ -7,7 +7,7 @@
  * @license GNU GPLv2
  * @link    https://github.com/gocom/rah_backup
  *
- * Copyright (C) 2013 Jukka Svahn <http://rahforum.biz>
+ * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
  * Licensed under GNU General Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
